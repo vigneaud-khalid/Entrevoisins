671c99536cd292c36932f47f1d3f04f295b8ee23
vigneaud-khalid <vigneaud40@yahoo.fr>
2022-03-05 23:51:51 +0100

    classes de test toujours en ROUGE

---
+++ app/src/androidTest/java/com/openclassrooms/entrevoisins/favorite_list/FavoritesListTest.java
@@ -0,0 +1,20 @@
+package com.openclassrooms.entrevoisins.favorite_list;
+
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.runner.RunWith;
+
+
+/**
+ * Created by ordinateur _ Khalid _  on 04/03/2022.
+ */
+
+
+
+/**
+ * Test class for list of neighbours
+ */
+@RunWith(AndroidJUnit4.class)
+public class FavoritesListTest {
+}
